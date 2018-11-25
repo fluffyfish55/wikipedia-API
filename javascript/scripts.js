61dba47d7a0122df8bf1265ae313b695ad3931d2
Fania Ra <fania@web.de>
2018-11-25 17:03:29 +0000

    fix url to match other branches

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -7,7 +7,7 @@
   let demoJSON = document.getElementById("demo");
 
   // get an api key from http://open-platform.theguardian.com/access/
-  let baseURL = "https://content.guardianapis.com/search?api-key=<YOUR-API-KEY-HERE>&q=";
+  let baseURL = "https://content.guardianapis.com/search?api-key=YOUR-API-KEY-HERE&q=";
 
   searchForm.addEventListener("submit", function(ev){
     let url = baseURL + queryBox.value;
